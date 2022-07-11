b143ff579e8c3528267c431dd4e3490372cd1f13
akshay-ugale <ugaleakshay07@gmail.com>
2022-07-11 13:39:31 +0530

    added Movies MVC

---
+++ VideoRental/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoRental.Models;
+
+namespace VideoRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name ="Shrek!"};
+            return View(movie);
+        }
+    }
+}
